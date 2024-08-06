8ed7f9b97104c5d3007e48c562ea95ce912541a7
Codezz-ops <codezz-ops@protonmail.com>
2024-08-06 09:06:19 -0400

    added var block to store all maps and re-coded maps

--- lexer/maps.go
+++ lexer/maps.go
@@ -2,194 +2,145 @@ package lexer
 
 import "fmt"
 
-var operatorMap = map[byte]TokenType{
-	'=': ASSIGN,
-	'!': NOT,
-	'+': PLUS,
-	'-': MINUS,
-	'*': MULTIPLY,
-	'/': DIVIDE,
-	'<': LT,
-	'>': GT,
-	'|': PIPE,
-	'(': OPEN_PAREN,
-	')': CLOSE_PAREN,
-	'[': OPEN_BRACKET,
-	']': CLOSE_BRACKET,
-	'{': OPEN_BRACE,
-	'}': CLOSE_BRACE,
-	'.': DOT,
-	'?': QUESTION,
-	':': COLON,
-	';': SEMI_COLON,
-	',': COMMA,
-	'"': TYPE_STRING,
-}
+var (
+	operatorMap = map[byte]TokenType{
+		'=': ASSIGN,
+		'!': NOT,
+		'+': PLUS,
+		'-': MINUS,
+		'*': MULTIPLY,
+		'/': DIVIDE,
+		'<': LT,
+		'>': GT,
+		'|': PIPE,
+		'(': OPEN_PAREN,
+		')': CLOSE_PAREN,
+		'[': OPEN_BRACKET,
+		']': CLOSE_BRACKET,
+		'{': OPEN_BRACE,
+		'}': CLOSE_BRACE,
+		'.': DOT,
+		'?': QUESTION,
+		':': COLON,
+		';': SEMI_COLON,
+		',': COMMA,
+		'"': TYPE_STRING,
+	}
 
-var assignmentMap = map[byte]TokenType{
-	'=': EQUALS,
-	'+': PLUS_ASSIGN,
-	'-': MINUS_ASSIGN,
-	'*': MULTIPLY_ASSIGN,
-	'/': DIVIDE_ASSIGN,
-	'<': LESS_THAN_EQUALS,
-	'>': GREATER_THAN_EQUALS,
-	'|': LOGICAL_OR,
-}
+	assignmentMap = map[byte]TokenType{
+		'=': EQUALS,
+		'+': PLUS_ASSIGN,
+		'-': MINUS_ASSIGN,
+		'*': MULTIPLY_ASSIGN,
+		'/': DIVIDE_ASSIGN,
+		'<': LESS_THAN_EQUALS,
+		'>': GREATER_THAN_EQUALS,
+		'|': LOGICAL_OR,
+	}
 
-var keywordMap = map[string]TokenType{
-	"int":      TYPE_INT,
-	"float":    TYPE_FLOAT,
-	"bool":     TYPE_BOOLEAN,
-	"string":   TYPE_STRING,
-	"try":      KEYWORDS_TRY,
-	"catch":    KEYWORDS_CATCH,
-	"import":   KEYWORDS_IMPORT,
-	"true":     KEYWORDS_TRUE,
-	"false":    KEYWORDS_FALSE,
-	"function": KEYWORDS_FUNCTION,
-	"if":       KEYWORDS_IF,
-	"let":      KEYWORDS_VAR,
-	"const":    KEYWORDS_CONST,
-	"else":     KEYWORDS_ELSE,
-	"return":   KEYWORDS_RETURN,
-	"for":      KEYWORDS_FOR,
-	"switch":   KEYWORDS_SWITCH,
-	"case":     KEYWORDS_CASE,
-	"default":  KEYWORDS_DEFAULT,
-}
+	keywordMap = map[string]TokenType{
+		"int":      TYPE_INT,
+		"float":    TYPE_FLOAT,
+		"bool":     TYPE_BOOLEAN,
+		"string":   TYPE_STRING,
+		"try":      KEYWORDS_TRY,
+		"catch":    KEYWORDS_CATCH,
+		"import":   KEYWORDS_IMPORT,
+		"true":     KEYWORDS_TRUE,
+		"false":    KEYWORDS_FALSE,
+		"function": KEYWORDS_FUNCTION,
+		"if":       KEYWORDS_IF,
+		"let":      KEYWORDS_VAR,
+		"const":    KEYWORDS_CONST,
+		"else":     KEYWORDS_ELSE,
+		"return":   KEYWORDS_RETURN,
+		"for":      KEYWORDS_FOR,
+		"switch":   KEYWORDS_SWITCH,
+		"case":     KEYWORDS_CASE,
+		"default":  KEYWORDS_DEFAULT,
+	}
+
+	tokenTypeNames = map[TokenType]string{
+		EOF:                 "EOF",
+		ERROR:               "ERROR",
+		MINUS:               "MINUS",
+		PLUS:                "PLUS",
+		MULTIPLY:            "MULTIPLY",
+		DIVIDE:              "DIVIDE",
+		OPEN_PAREN:          "OPEN_PAREN",
+		CLOSE_PAREN:         "CLOSE_PAREN",
+		OPEN_BRACKET:        "OPEN_BRACKET",
+		CLOSE_BRACKET:       "CLOSE_BRACKET",
+		OPEN_BRACE:          "OPEN_BRACE",
+		CLOSE_BRACE:         "CLOSE_BRACE",
+		DOT:                 "DOT",
+		QUESTION:            "QUESTION",
+		COLON:               "COLON",
+		SEMI_COLON:          "SEMI_COLON",
+		COMMA:               "COMMA",
+		NOT:                 "NOT",
+		PIPE:                "PIPE",
+		TILDE:               "TILDE",
+		AMSPERSAND:          "AMSPERSAND",
+		GT:                  "GT",
+		LT:                  "LT",
+		LOGICAL_OR:          "LOGICAL_OR",
+		LOGICAL_AND:         "LOGICAL_AND",
+		EQUALS:              "EQUALS",
+		NOT_EQUALS:          "NOT_EQUALS",
+		LESS_THAN_EQUALS:    "LESS_THAN_EQUALS",
+		GREATER_THAN_EQUALS: "GREATER_THAN_EQUALS",
+		ASSIGN:              "ASSIGN",
+		PLUS_ASSIGN:         "PLUS_ASSIGN",
+		MINUS_ASSIGN:        "MINUS_ASSIGN",
+		MULTIPLY_ASSIGN:     "MULTIPLY_ASSIGN",
+		DIVIDE_ASSIGN:       "DIVIDE_ASSIGN",
+		IDENTIFIER:          "IDENTIFIER",
+		TYPE_STRING:         "TYPE_STRING",
+		TYPE_INT:            "TYPE_INT",
+		TYPE_FLOAT:          "TYPE_FLOAT",
+		TYPE_BOOLEAN:        "TYPE_BOOLEAN",
+		KEYWORDS_FUNCTION:   "KEYWORDS_FUNCTION",
+		KEYWORDS_IF:         "KEYWORDS_IF",
+		KEYWORDS_ELSE:       "KEYWORDS_ELSE",
+		KEYWORDS_RETURN:     "KEYWORDS_RETURN",
+		KEYWORDS_VAR:        "KEYWORDS_VAR",
+		KEYWORDS_CONST:      "KEYWORDS_CONST",
+		KEYWORDS_SWITCH:     "KEYWORDS_SWITCH",
+		KEYWORDS_FOR:        "KEYWORDS_FOR",
+		KEYWORDS_CASE:       "KEYWORDS_CASE",
+		KEYWORDS_DEFAULT:    "KEYWORDS_DEFAULT",
+		KEYWORDS_TRUE:       "KEYWORDS_TRUE",
+		KEYWORDS_FALSE:      "KEYWORDS_FALSE",
+		KEYWORDS_TRY:        "KEYWORDS_TRY",
+		KEYWORDS_CATCH:      "KEYWORDS_CATCH",
+		KEYWORDS_IMPORT:     "KEYWORDS_IMPORT",
+	}
+)
 
 func TokenTypeToString(t TokenType) string {
-	switch t {
-	case EOF:
-		return "EOF"
-	case ERROR:
-		return "ERROR"
-	case MINUS:
-		return "MINUS"
-	case PLUS:
-		return "PLUS"
-	case MULTIPLY:
-		return "MULTIPLY"
-	case DIVIDE:
-		return "DIVIDE"
-	case OPEN_PAREN:
-		return "OPEN_PAREN"
-	case CLOSE_PAREN:
-		return "CLOSE_PAREN"
-	case OPEN_BRACKET:
-		return "OPEN_BRACKET"
-	case CLOSE_BRACKET:
-		return "CLOSE_BRACKET"
-	case OPEN_BRACE:
-		return "OPEN_BRACE"
-	case CLOSE_BRACE:
-		return "CLOSE_BRACE"
-	case DOT:
-		return "DOT"
-	case QUESTION:
-		return "QUESTION"
-	case COLON:
-		return "COLON"
-	case SEMI_COLON:
-		return "SEMI_COLON"
-	case COMMA:
-		return "COMMA"
-	case NOT:
-		return "NOT"
-	case PIPE:
-		return "PIPE"
-	case TILDE:
-		return "TILDE"
-	case AMSPERSAND:
-		return "AMSPERSAND"
-	case GT:
-		return "GT"
-	case LT:
-		return "LT"
-	case LOGICAL_OR:
-		return "LOGICAL_OR"
-	case LOGICAL_AND:
-		return "LOGICAL_AND"
-	case EQUALS:
-		return "EQUALS"
-	case NOT_EQUALS:
-		return "NOT_EQUALS"
-	case LESS_THAN_EQUALS:
-		return "LESS_THAN_EQUALS"
-	case GREATER_THAN_EQUALS:
-		return "GREATER_THAN_EQUALS"
-	case ASSIGN:
-		return "ASSIGN"
-	case PLUS_ASSIGN:
-		return "PLUS_ASSIGN"
-	case MINUS_ASSIGN:
-		return "MINUS_ASSIGN"
-	case MULTIPLY_ASSIGN:
-		return "MULTIPLY_ASSIGN"
-	case DIVIDE_ASSIGN:
-		return "DIVIDE_ASSIGN"
-	case IDENTIFIER:
-		return "IDENTIFIER"
-	case TYPE_STRING:
-		return "TYPE_STRING"
-	case TYPE_INT:
-		return "TYPE_INT"
-	case TYPE_FLOAT:
-		return "TYPE_FLOAT"
-	case TYPE_BOOLEAN:
-		return "TYPE_BOOLEAN"
-	case KEYWORDS_FUNCTION:
-		return "KEYWORDS_FUNCTION"
-	case KEYWORDS_IF:
-		return "KEYWORDS_IF"
-	case KEYWORDS_ELSE:
-		return "KEYWORDS_ELSE"
-	case KEYWORDS_RETURN:
-		return "KEYWORDS_RETURN"
-	case KEYWORDS_VAR:
-		return "KEYWORDS_VAR"
-	case KEYWORDS_CONST:
-		return "KEYWORDS_CONST"
-	case KEYWORDS_SWITCH:
-		return "KEYWORDS_SWITCH"
-	case KEYWORDS_FOR:
-		return "KEYWORDS_FOR"
-	case KEYWORDS_CASE:
-		return "KEYWORDS_CASE"
-	case KEYWORDS_DEFAULT:
-		return "KEYWORDS_DEFAULT"
-	case KEYWORDS_TRUE:
-		return "KEYWORDS_TRUE"
-	case KEYWORDS_FALSE:
-		return "KEYWORDS_FALSE"
-	case KEYWORDS_TRY:
-		return "KEYWORDS_TRY"
-	case KEYWORDS_CATCH:
-		return "KEYWORDS_CATCH"
-	case KEYWORDS_IMPORT:
-		return "KEYWORDS_IMPORT"
-	default:
-		return fmt.Sprintf("UNKNOWN(%d)", t)
+	if name, exists := tokenTypeNames[t]; exists {
+		return name
 	}
+	return fmt.Sprintf("UNKNOWN(%d)", t)
 }
 
 func ToKeywords(value string) TokenType {
-	if val, ok := keywordMap[value]; ok {
-		return val
+	if tokenType, exists := keywordMap[value]; exists {
+		return tokenType
 	}
 	return IDENTIFIER
 }
 
 func GetOperatorType(char byte) TokenType {
-	if tokenType, ok := operatorMap[char]; ok {
+	if tokenType, exists := operatorMap[char]; exists {
 		return tokenType
 	}
 	return ERROR
 }
 
 func GetAssignmentType(char byte) TokenType {
-	if tokenType, ok := assignmentMap[char]; ok {
+	if tokenType, exists := assignmentMap[char]; exists {
 		return tokenType
 	}
 	return ERROR
